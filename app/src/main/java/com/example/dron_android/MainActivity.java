35c463c1b8c28b0754a78120ed2f9d1e799a6966
Yuu <s13t232@stmail.eng.kagawa-u.ac.jp>
2015-11-19 13:42:59 +0900

    Make activity_main.xml

---
+++ app/src/main/java/com/example/dron_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dron_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.WindowDecorActionBar;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
