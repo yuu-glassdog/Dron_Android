13f438a3045f71d552241a18e6b21064f1149924
Yuu <s13t232@stmail.eng.kagawa-u.ac.jp>
2015-11-28 02:09:01 +0900

    Make TitleActivity and activate intent

--- app/src/main/java/com/example/dron_android/TitleActivity.java
+++ app/src/main/java/com/example/dron_android/TitleActivity.java
@@ -2,12 +2,27 @@ package com.example.dron_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class TitleActivity extends AppCompatActivity {
+public class TitleActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        ImageButton imb = (ImageButton)findViewById(R.id.imageView);
+        imb.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        // ゲーム画面を起動
+        Intent intent = new Intent(TitleActivity.this, MainActivity.class);
+        startActivity(intent);
+        // 画面移動後、タイトル画面を消去
+        finish();
     }
 }
